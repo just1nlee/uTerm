d0a6cc14f6a109a229a376aaf05530c36969c99d
tobias2004 <tobiiascr7@hotmail.com>
2025-04-06 01:00:50 -0700

    Fixed cors

--- backend/portal.py
+++ backend/portal.py
@@ -35,7 +35,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://universeterminal.com", "http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "DELETE"],
+    allow_methods=["GET", "POST", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization", "X-API-Key"])
 
 
